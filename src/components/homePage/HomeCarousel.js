349ce4db4453f2d1de25a15094371025af8e44cf
minirei <minrei101@gmail.com>
2021-11-05 19:10:59 +0800

    added arrows

--- src/components/homePage/HomeCarousel.js
+++ src/components/homePage/HomeCarousel.js
@@ -1,17 +1,16 @@
 import { motion, AnimatePresence } from "framer-motion"
-import React from "react"
+import React, { useState } from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { useStaticQuery, graphql } from "gatsby"
 
 // Styles
-import { HomeCarouselSection } from "../../styles/carouselStyles"
-import { cover } from "jimp"
+import { HomeCarouselSection, CarouselNav } from "../../styles/carouselStyles"
 
 const HomeCarousel = () => {
   const { launch, party, kitchen, supply, wars } = useStaticQuery(
     graphql`
       query {
-        launch: file(relativePath: { eq: "roadmap-launch.png" }) {
+        launch: file(relativePath: { eq: "roadmap-launch.jpeg" }) {
           childImageSharp {
             gatsbyImageData(
               quality: 100
@@ -20,7 +19,7 @@ const HomeCarousel = () => {
             )
           }
         }
-        party: file(relativePath: { eq: "roadmap-party.png" }) {
+        party: file(relativePath: { eq: "roadmap-party.jpeg" }) {
           childImageSharp {
             gatsbyImageData(
               quality: 100
@@ -29,7 +28,7 @@ const HomeCarousel = () => {
             )
           }
         }
-        kitchen: file(relativePath: { eq: "roadmap-kitchen.png" }) {
+        kitchen: file(relativePath: { eq: "roadmap-kitchen.jpeg" }) {
           childImageSharp {
             gatsbyImageData(
               quality: 100
@@ -38,7 +37,7 @@ const HomeCarousel = () => {
             )
           }
         }
-        supply: file(relativePath: { eq: "roadmap-supply.png" }) {
+        supply: file(relativePath: { eq: "roadmap-supply.jpeg" }) {
           childImageSharp {
             gatsbyImageData(
               quality: 100
@@ -60,8 +59,19 @@ const HomeCarousel = () => {
     `
   )
 
-  const images = [launch, party, kitchen, supply, wars]
-  const image = getImage(images[1])
+  const images = [
+    getImage(launch),
+    getImage(party),
+    getImage(kitchen),
+    getImage(supply),
+    getImage(wars),
+  ]
+  // const image = getImage(images[1])
+
+  const [[card, direction], setCard] = useState([0, 0])
+  const paginate = newDirection => {
+    setPage([page + newDirection, newDirection])
+  }
 
   return (
     <>
@@ -69,11 +79,47 @@ const HomeCarousel = () => {
         <div className="wrapper">
           <GatsbyImage
             className="image"
-            image={image}
+            image={images[card]}
             alt=""
             objectPosition="50% 100%"
           ></GatsbyImage>
         </div>
+        <CarouselNav>
+          <motion.div className="prev">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="450.284 152.487 391.604 290.307"
+              enable-background="new 450.284 152.487 391.604 290.307"
+            >
+              <g transform="translate(0 -37)">
+                <polygon
+                  fill="#FFFFFF"
+                  points="450.284,311.096 755.958,311.096 667.735,208.821 690.465,189.487 841.889,323.405 
+		841.889,345.875 690.465,479.794 667.735,460.459 755.958,357.836 450.284,357.836 	"
+                />
+              </g>
+            </svg>
+          </motion.div>
+          <motion.div className="next">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="450.284 152.487 391.604 290.307"
+              enable-background="new 450.284 152.487 391.604 290.307"
+            >
+              <g transform="translate(0 -37)">
+                <polygon
+                  fill="#FFFFFF"
+                  points="450.284,311.096 755.958,311.096 667.735,208.821 690.465,189.487 841.889,323.405 
+		841.889,345.875 690.465,479.794 667.735,460.459 755.958,357.836 450.284,357.836 	"
+                />
+              </g>
+            </svg>
+          </motion.div>
+        </CarouselNav>
       </HomeCarouselSection>
     </>
   )
--- src/styles/carouselStyles.js
+++ src/styles/carouselStyles.js
@@ -2,17 +2,50 @@ import styled from "styled-components"
 import { motion } from "framer-motion"
 
 export const HomeCarouselSection = styled(motion.div)`
-  height: 120vh;
+  height: 100vh;
   width: 100vw;
   /* padding-bottom: 10vh; */
   position: relative;
-  /* overflow: hidden; */
+  overflow: hidden;
   .wrapper {
     height: 100%;
     width: 100%;
     .image {
-      max-width: 100%;
-      max-height: 100%;
+      /* max-width: 100%; */
+      height: 100%;
     }
+  }
+`
+
+export const CarouselNav = styled(motion.div)`
+  position: absolute;
+  bottom: 10%;
+  right: 5%;
+  display: flex;
+  .prev,
+  .next {
+    width: 4vw;
+    height: 4vw;
+    border-radius: 100%;
+    display: inline-block;
+    border: 1px solid rgb(235, 235, 235);
+    margin-right: 4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .prev {
+    svg {
+      transform: rotate(180deg);
+    }
+  }
+
+  .next {
+    margin-right: 0;
+  }
+
+  svg {
+    height: 50%;
   }
 `
