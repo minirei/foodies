9306b44b6aaf5237b2b07ef783aabeb1faa19e10
minirei <minrei101@gmail.com>
2021-11-05 16:01:45 +0800

    objectPosition set to 0% 100% to keep img centered at bottom

--- src/components/homePage/HomeCarousel.js
+++ src/components/homePage/HomeCarousel.js
@@ -5,12 +5,49 @@ import { useStaticQuery, graphql } from "gatsby"
 
 // Styles
 import { HomeCarouselSection } from "../../styles/carouselStyles"
+import { cover } from "jimp"
 
 const HomeCarousel = () => {
-  const { launch } = useStaticQuery(
+  const { launch, party, kitchen, supply, wars } = useStaticQuery(
     graphql`
       query {
-        launch: file(relativePath: { eq: "hero.jpeg" }) {
+        launch: file(relativePath: { eq: "roadmap-launch.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 100
+              width: 2000
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+        party: file(relativePath: { eq: "roadmap-party.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 100
+              width: 2000
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+        kitchen: file(relativePath: { eq: "roadmap-kitchen.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 100
+              width: 2000
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+        supply: file(relativePath: { eq: "roadmap-supply.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 100
+              width: 2000
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+        wars: file(relativePath: { eq: "roadmap-wars.png" }) {
           childImageSharp {
             gatsbyImageData(
               quality: 100
@@ -23,14 +60,19 @@ const HomeCarousel = () => {
     `
   )
 
-  //   const images = [launch, party, kitchen, supply, wars]
-  const image = getImage(launch)
+  const images = [launch, party, kitchen, supply, wars]
+  const image = getImage(images[1])
 
   return (
     <>
       <HomeCarouselSection>
         <div className="wrapper">
-          <GatsbyImage className="image" image={image} alt=""></GatsbyImage>
+          <GatsbyImage
+            className="image"
+            image={image}
+            alt=""
+            objectPosition="50% 100%"
+          ></GatsbyImage>
         </div>
       </HomeCarouselSection>
     </>
--- src/styles/carouselStyles.js
+++ src/styles/carouselStyles.js
@@ -2,15 +2,16 @@ import styled from "styled-components"
 import { motion } from "framer-motion"
 
 export const HomeCarouselSection = styled(motion.div)`
-  height: 80vh;
+  height: 120vh;
   width: 100vw;
+  /* padding-bottom: 10vh; */
   position: relative;
-  background-color: blue;
+  /* overflow: hidden; */
   .wrapper {
     height: 100%;
     width: 100%;
     .image {
-      width: 100%;
+      max-width: 100%;
       max-height: 100%;
     }
   }
