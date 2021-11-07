864a52f25de5053d8605646642d25ac10784c74b
minirei <minrei101@gmail.com>
2021-11-07 18:32:34 +0800

    testing drag

--- src/components/homePage/HomeCarousel.js
+++ src/components/homePage/HomeCarousel.js
@@ -43,6 +43,12 @@ const cardAnimation = {
   },
 }
 
+// Mobile Swiping
+const swipeConfidenceThreshold = 10000
+const swipePower = (offset, velocity) => {
+  return Math.abs(offset) * velocity
+}
+
 const HomeCarousel = () => {
   const { isMobile } = useGlobalStateContext()
   const { launch, party, kitchen, supply, wars, charity, future } =
@@ -148,6 +154,17 @@ const HomeCarousel = () => {
             initial="enter"
             animate="center"
             exit="exit"
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x)
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1)
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1)
+              }
+            }}
           >
             <GatsbyImage
               className="image"
